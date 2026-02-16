07a6c3c04cc89343b80bc01d1f7e1f37d9abb5c7
ArticProject <mihailulybin67@gmail.com>
2026-02-16 23:26:32 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,39 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'screens/home_screen.dart';
-import 'onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'screens/onboarding_screen.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: VPNApp(),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
     ),
   );
+  runApp(const ArcticVPN());
 }
 
-class VPNApp extends StatelessWidget {
-  const VPNApp({super.key});
+class ArcticVPN extends StatelessWidget {
+  const ArcticVPN({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       title: 'Arctic VPN',
       debugShowCheckedModeBanner: false,
-      theme: const CupertinoThemeData(
+      theme: ThemeData(
+        useMaterial3: true,
         brightness: Brightness.light,
-        primaryColor: Color(0xFF007AFF),
+        scaffoldBackgroundColor: const Color(0xFFF2F2F7),
+        fontFamily: '.SF Pro Display',
       ),
-      home: const SplashCheck(),
-      routes: {
-        '/home': (context) => const HomeScreen(),
-        '/onboarding': (context) => const OnboardingScreen(),
-      },
-    );
-  }
-}
-
-// ПРОВЕРКА ОНБОРДИНГА
-class SplashCheck extends StatefulWidget {
-  const SplashCheck({super.key});
-
-  @override
-  State<SplashCheck> createState() => _SplashCheckState();
-}
-
-class _SplashCheckState extends State<SplashCheck> {
-  @override
-  void initState() {
-    super.initState();
-    _checkOnboarding();
-  }
-
-  Future<void> _checkOnboarding() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    final prefs = await SharedPreferences.getInstance();
-    final completed = prefs.getBool('onboarding_completed') ?? false;
-
-    if (mounted) {
-      Navigator.of(context).pushReplacementNamed(
-        completed ? '/home' : '/onboarding',
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      backgroundColor: Color(0xFF1A3A52),
-      child: Center(
-        child: Icon(
-          CupertinoIcons.snow,
-          size: 80,
-          color: Color(0xFF64B5F6),
-        ),
-      ),
+      home: const OnboardingScreen(),
     );
   }
 }
